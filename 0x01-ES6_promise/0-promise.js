2ba890a79f3f7b2cf3898dcce62c914b7e81c91c
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-09-28 00:24:01 +0100

    0-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,8 @@
-function getResponseFromAPI() {
-  return true;
+// 0-promise.js
+export function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Resolve the promise with the value true
+    resolve(true);
+  });
 }
 
