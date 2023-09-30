cfe0df26552a574747304d9d9193fbb08f0128f2
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-09-30 09:10:10 +0100

    2-arrow

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = add(newNeighborhood) => {
+  self.addNeighborhood = (newNeighborhood) => {
 	// Arrow function equivalent of add function
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
