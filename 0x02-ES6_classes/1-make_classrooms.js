08d1a385c4bc126a8860c94743cc1cfaa16dc75e
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-01 20:41:00 +0100

    1-make_classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,5 @@
 import ClassRoom from './0-classroom.js';
 
-/**
- * Function to implement ClassRoom function
- * It should return an array of 3 ClassRoom objects
- * with the sizes 19, 20, and 34 (in this order)
- */
 export default function initializeRooms() {
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
-  return rooms;
+  return Array.from([19, 20, 34], size => new ClassRoom(size));
 }
