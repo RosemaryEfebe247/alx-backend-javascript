592c635c89248f07defd7d031ec1da0a60518ef5
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-01 20:56:26 +0100

    1-make_classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,7 +2,7 @@ import ClassRoom from './0-classroom.js';
 
 /**
  * initializeRooms - Function that initialize rooms
- * @return: Return array of sizes
+ * @returns an array of sizes
  */
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
