f5dfc046b727a1984f596e04a600c1c65e13e5c7
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-02 23:24:34 +0100

    get_list_students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+export default function getListStudents() {
+  const ids = [1, 2, 5];
+  const firstNames = ['Guillaume', 'James', 'Serena'];
+  const locations = ['San Francisco', 'Columbia', 'San Francisco'];
+  
+  return ids.map((id, index) => ({
+    id,
+    firstName: firstNames[index]
+    locaTion: locations[index]
+  }))
+}
---
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -0,0 +1,3 @@
+const getListStudents = require("./0-get_list_students.js");
+
+console.log(getListStudents());
