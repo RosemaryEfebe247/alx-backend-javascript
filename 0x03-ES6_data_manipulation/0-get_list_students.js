c6b2ee0a30b71a386d1f8b805627e8e16bec73fc
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-02 23:31:53 +0100

    get_list_students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,8 @@
+/**
+ * Retrieves a list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
   const ids = [1, 2, 5];
   const firstNames = ['Guillaume', 'James', 'Serena'];
