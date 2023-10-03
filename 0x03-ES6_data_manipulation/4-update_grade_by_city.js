41400559f9f9f430990c2089ddef6751cf2b5981
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-03 03:40:42 +0100

    update_grade_by_city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,26 @@
 /**
- * Function that filters by city and append grade
+ * Function that filters by city and appends grade
  * returns the filtered student with the new grade
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
   if (students instanceof Array) {
     const cityfilter = students.filter((student) => student.location === city);
-	if (Array.isArray(newGrades)) {  
-      return cityfilter.map((student, index) => ({
-	  ...student,
-	  grade: newGrades[index] && newGrades[index].grade !== null ? newGrades[index].grade : 'N/A',
-	}));
-   }
+    if (Array.isArray(newGrades)) {
+      return cityfilter.map((student) => {
+        const matchgrade = newGrades.find((findgrade) => findgrade.studentId === student.id);
+        if (matchgrade && matchgrade.grade) {
+          return {
+            ...student,
+            grade: matchgrade.grade,
+          };
+        } else {
+          return {
+            ...student,
+            grade: 'N/A',
+          };
+        }
+      });
+    }
   }
   return [];
 }
