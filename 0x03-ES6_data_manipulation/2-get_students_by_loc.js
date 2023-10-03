c33bc070966779cb66934fbdcb8704752da0d18b
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-03 01:07:29 +0100

    2-get_students_by_loc

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+/**
+ * Function that returns a filtered item
+ * @return the filtered object
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
