4749a9998a8bd3043083bda8e6fd47ed0cbbed14
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-03 03:43:51 +0100

    update_grade_by_city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,16 @@
 /**
- * Function that filters by city and appends grade
- * returns the filtered student with the new grade
+ * Updates the grades of a list of students in a given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {*} city - The city of students.
+ * @param {{
+ *   studentId: Number,
+ *   grade: Number,
+ * }[]} newGrades - The new grades to be given to a student.
+ * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
   if (students instanceof Array) {
