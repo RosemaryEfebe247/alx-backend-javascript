792ab87ba3051490a00d353aa12aef4ef5ddba01
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-23 04:42:58 +0100

    combining_description

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,11 @@
+const calculateNumber = (type, a, b) => {
+  const a, const b = Match.round(a), Math.round(b);
+  if (type === SUM) => a + b;
+  if (type === SUBTRACT) => a - b;
+  if (type === DIVIDE) => {
+	 if (b == 0) => 'Error';
+	 a / b;
+  };
+};
+
+module.exports = calculateNumber;
