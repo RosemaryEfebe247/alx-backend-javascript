304fee3795c240074f28ac33f6a2f22cacb39398
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-24 05:39:59 +0100

    combining_descr

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,11 +1,17 @@
-const calculateNumber = (type, a, b) => {
-  const a, const b = Match.round(a), Math.round(b);
-  if (type === SUM) => a + b;
-  if (type === SUBTRACT) => a - b;
-  if (type === DIVIDE) => {
-	 if (b == 0) => 'Error';
-	 a / b;
-  };
-};
+const calculateNumber = require("./1-calcul.js");
+const assert = require('assert');
 
-module.exports = calculateNumber;
+describe("Math Computation", () => {
+  it("Sum", () => {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+  it("Subtract", () => {
+	assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+  it("Divide", () => {
+	assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it("Divide with zero denominator", () => {
+	assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
